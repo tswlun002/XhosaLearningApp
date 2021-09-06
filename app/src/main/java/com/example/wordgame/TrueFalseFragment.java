126021ca06824e874c56e8556fc6542d4adf9a01
Kuhle Mxotwa <mxtkuh001@cs.uct.ac.za>
2021-09-06 16:26:22 +0200

    add method in Activity results class to generate correct answers for the user

---
+++ app/src/main/java/com/example/wordgame/TrueFalseFragment.java
@@ -0,0 +1,156 @@
+package com.example.wordgame;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.wordgame.databinding.FragmentTrueBinding;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TrueFalseFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TrueFalseFragment extends Fragment  {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private int pos;
+
+    //Ini
+    @SuppressLint("StaticFieldLeak")
+    static  TextView question ;
+
+
+    private final String[] questions = {
+
+            "The animal name on the picture is  Inkomo?",
+            "The animal name on the picture is Igusha?",
+            "The animal name on the picture is Ikati?",
+            "The animal name on the picture is Ibhokhwe?"
+    };
+
+    private final int[] figure = {
+            R.drawable.igusha, R.drawable.igusha,
+            R.drawable.igusha, R.drawable.igusha
+    };
+
+
+    private FragmentTrueBinding binding;
+    private LayoutInflater inflater;
+
+    public TrueFalseFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TrueFalseFragment newInstance(String param1, String param2) {
+        TrueFalseFragment fragment = new TrueFalseFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    /**
+     * creates TrueFalseFragment
+     * @param inflater of TrueFalseFragment
+     * @param container of TrueFalseFragment
+     * @param savedInstanceState for TrueFalseFragment
+     * @return
+     */
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentTrueBinding.inflate(inflater, container, false);
+        this.inflater = inflater;
+        setUpRecycleView(binding);
+        return  binding.getRoot();
+
+    }
+
+    /**
+     * helper method to set up controller
+     * @param binding  FragmentTrueBinding binder
+     */
+    private  void setUpRecycleView(FragmentTrueBinding binding){
+        TrueFalseController trueFalseController = new TrueFalseController(requireContext(),questions,figure,R.layout.true_false_adapter);
+        binding.listviewIDtrueAdapter.setAdapter(trueFalseController);
+        binding.listviewIDtrueAdapter.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+    }
+
+
+    /**
+     * created view for TrueFalseFragment
+     * @param view of TrueFalseFragment
+     * @param savedInstanceState for  TrueFalseFragment
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        handleButton();
+        ((MainActivity) requireActivity()).backUpPressed(TrueFalseFragment.this,R.id.action_trueFalseFragment_to_play);
+
+    }
+    /**
+     * handle button event
+     * Show grades for user
+     */
+    private  void handleButton() {
+
+        binding.submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActiviyResults activiyResults = new ActiviyResults(inflater, requireContext());
+                activiyResults.gradesActity(18,20);
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/wordgame/presentation_layer/ActivityResults.java
+++ app/src/main/java/com/example/wordgame/presentation_layer/ActivityResults.java
@@ -1,90 +1,55 @@
-package com.example.wordgame.presentation_layer;
+package com.example.wordgame;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.navigation.Navigation;
+import java.util.zip.Inflater;
 
-import com.example.wordgame.R;
-
-import java.util.HashMap;
-import java.util.List;
-
-public abstract  class ActivityResults {
+public class ActiviyResults {
 
     private final LayoutInflater inflater;
     private final Context context;
-    private  int actionId;
-    private final View fragmentView;
 
-    public ActivityResults(LayoutInflater inflater, int Id, View fragment1){
+    public  ActiviyResults (LayoutInflater inflater,Context context){
         this.inflater=inflater;
-        this.context=inflater.getContext();
-        actionId = Id;
-        fragmentView = fragment1;
+        this.context=context;
     }
 
 
     /**
-     * This method display user score, the answers they had wrong with corrections.
+     * This method desplay's the user score, the answers they had wrong with corrections.
      * @param userScore stores the user score for each of the exercises
      * @param totalScore stores the user average score of each of the levels.
      */
     @SuppressLint("SetTextI18n")
     //introduced new parameters userScore and total Score
-    public void gradesActity(double userScore, int totalScore, List<String> correctAnswers){
+    public void gradesActity(int userScore, int totalScore){
         final View view = inflater.inflate(R.layout.fragment_results__current_activity, null);
         AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-        //alertDialog.setTitle("     Score");
+        alertDialog.setTitle("                           Score");
 
 
-        String CorrectAnswers = buildAnswers(correctAnswers);
         TextView grades  = view.findViewById(R.id.gradesActivity);
         TextView answers = view.findViewById(R.id.CorrectionsID);
-        Button tryAgainButton = view.findViewById(R.id.ExitBitton);
-        Button nextButton = view.findViewById(R.id.NextButtonId);
-
-        grades.setText(CorrectAnswers);
+        Button button = view.findViewById(R.id.ExitBitton);
+        grades.setText("1.True\n2.false\n3.true\n4.false");
         answers.setText("Your Score and Answers:".toUpperCase()+userScore+"/"+totalScore);
         alertDialog.setView(view);
-        alertDialog.setCanceledOnTouchOutside(false);
         alertDialog.show();
-
-        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 Navigation.findNavController((MainActivity) inflater.getContext(),
-                         R.id.nav_host_fragment_content_main).
-                         navigate(actionId);
 
                 alertDialog.dismiss();
-
             }
         });
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alertDialog.dismiss();
-                Navigation.findNavController((MainActivity) inflater.getContext(),
-                        R.id.nav_host_fragment_content_main).
-                        navigate(R.id.action_results_CurrentActivity_to_play);
-            }
-        });
-    }
-    String  buildAnswers( List<String> correctAnswers){
-        StringBuilder answers = new StringBuilder();
-        int count =0;
-        for (Object key:correctAnswers) {
-            answers.append(count).append(".").append(key).append("\n");
-            count++;
-        }
-        return answers.toString();
     }
 
 }
