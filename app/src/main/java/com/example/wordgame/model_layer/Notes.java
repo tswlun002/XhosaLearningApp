74eae526b2e53d4c30304b8da04a033467b02ef9
Kuhle Mxotwa <mxtkuh001@cs.uct.ac.za>
2021-09-16 17:09:45 +0200

    add method in Activity results class to clear editText after user has submitted

--- app/src/main/java/com/example/wordgame/model_layer/Notes.java
+++ app/src/main/java/com/example/wordgame/model_layer/Notes.java
@@ -1,4 +1,46 @@
 package com.example.wordgame.model_layer;
 
 public class Notes {
+    private int notesid;
+    private  String section;
+    private  int level;
+
+    public int getNotesid() {
+        return notesid;
+    }
+
+    public void setNotesid(int notesId, String section, int level) {
+        this.notesid = notesId;
+        this.level=level;
+        this.section=section;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    @Override
+    public String toString() {
+        return "Notes{" +
+                "notesId=" + notesid +
+                ", section='" + section + '\'' +
+                ", level=" + level +
+                '}';
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Notes(int notesid) {
+        this.notesid = notesid;
+    }
 }
